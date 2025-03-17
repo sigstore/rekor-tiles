49835c8587728a3949deeea13ae474b33a855ce0
Colleen Murphy <colleenmurphy@google.com>
2025-03-17 16:28:03 -0700

    Add rekor and config steps to docker compose
    Update the rekor service in compose.yml to reflect the new configuration
    options for the rekor service. Add a service to run some gcloud steps to
    set up the Spanner emulator database instance.
    
    This also changes the GCS emulator to run on another port to avoid
    conflicting with the Rekor HTTP unit tests which start a server on the
    same port.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ config/emulator_init.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Copyright 2025 The Sigstore Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eu
+
+gcloud config configurations list | grep emulator | grep True || gcloud config configurations create emulator
+gcloud config set auth/disable_credentials true
+gcloud config set project $GCP_PROJECT
+gcloud config set api_endpoint_overrides/spanner $SPANNER_EMULATOR_REST_HOST
+
+gcloud spanner instances list | grep $SPANNER_INSTANCE || \
+    gcloud spanner instances create $SPANNER_INSTANCE \
+        --no-user-output-enabled \
+        --nodes=1 \
+        --description="test spanner instance for rekor tiles" \
+        --config=emulator-config
+gcloud spanner databases list --instance $SPANNER_INSTANCE | grep $SPANNER_DB || \
+    gcloud spanner databases create $SPANNER_DB \
+        --no-user-output-enabled \
+        --instance $SPANNER_INSTANCE
+
+echo "done" > /root/finished
