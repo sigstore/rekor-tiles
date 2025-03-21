cf59578ac7697c8ffdc078d7a61145f3bd0235e8
Colleen Murphy <colleenmurphy@google.com>
2025-03-21 14:38:49 -0700

    Return canonicalized body in server response
    Also add failure case to the unit test.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

--- pkg/tessera/tessera_test.go
+++ pkg/tessera/tessera_test.go
@@ -52,6 +52,7 @@ gb/AnEEsBNpTobDduU3OSNaiTp6liYf31FoE6AB/s8o=
 		expectLogIndex int64
 		expectTreeSize int64
 		expectHash     []byte
+		expectBody     []byte
 	}{
 		{
 			name: "success",
@@ -61,6 +62,14 @@ gb/AnEEsBNpTobDduU3OSNaiTp6liYf31FoE6AB/s8o=
 			expectLogIndex: int64(0),
 			expectTreeSize: int64(1),
 			expectHash:     []byte(tileHash),
+			expectBody:     []byte("stuff"),
+		},
+		{
+			name: "integration failed",
+			addFn: func(_ context.Context, _ *tessera.Entry) tessera.IndexFuture {
+				return func() (uint64, error) { return 0, fmt.Errorf("server error") }
+			},
+			expectErr: fmt.Errorf("add entry: await: server error"),
 		},
 	}
 	for _, test := range tests {
@@ -69,12 +78,13 @@ gb/AnEEsBNpTobDduU3OSNaiTp6liYf31FoE6AB/s8o=
 			got, gotErr := s.Add(ctx, entry)
 			if test.expectErr != nil {
 				assert.ErrorContains(t, gotErr, test.expectErr.Error())
-			} else {
-				assert.NoError(t, gotErr)
+				return
 			}
+			assert.NoError(t, gotErr)
 			assert.Equal(t, test.expectLogIndex, got.LogIndex)
 			assert.Equal(t, test.expectTreeSize, got.InclusionProof.TreeSize)
 			assert.Equal(t, test.expectHash, got.InclusionProof.RootHash)
+			assert.Equal(t, test.expectBody, got.CanonicalizedBody)
 		})
 	}
 }
