a32adbb56c2a047b6ccab4e04f5cb0b0aad5a735
Colleen Murphy <colleenmurphy@google.com>
2025-03-24 13:38:36 +0200

    verify: Add initial verification test from Colleen
    * Tests are roughly like TestInclusion TestCheckpoint in
      https://github.com/sigstore/rekor/blob/main/pkg/verify/verify_test.go
      (the entry content is from there as well)
    * The signed checkpoint envelope creation in TestVerifyCheckpoint is from
      https://github.com/transparency-dev/trillian-tessera/blob/ae724376e1ace4046767511c72c6006bde3ec87e/append_lifecycle.go#L298-L316
    
    Signed-off-by: Jussi Kukkonen <jkukkonen@google.com>

---
+++ pkg/verify/verify_test.go
@@ -0,0 +1,85 @@
+package verify
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"testing"
+
+	pbs "github.com/sigstore/protobuf-specs/gen/pb-go/rekor/v1"
+	rekornote "github.com/sigstore/rekor-tiles/pkg/note"
+	"github.com/sigstore/sigstore/pkg/signature"
+	"github.com/stretchr/testify/assert"
+	f_log "github.com/transparency-dev/formats/log"
+	note "golang.org/x/mod/sumdb/note"
+)
+
+func TestVerifyInclusionProof(t *testing.T) {
+	hash, err := hex.DecodeString("59a575f157274702c38de3ab1e1784226f391fb79500ebf9f02b4439fb77574c")
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootHash, err := hex.DecodeString("5be1758dd2228acfaf2546b4b6ce8aa40c82a3748f3dcb550e0d67ba34f02a45")
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, err := base64.StdEncoding.DecodeString("eyJhcGlWZXJzaW9uIjoiMC4wLjEiLCJraW5kIjoicmVrb3JkIiwic3BlYyI6eyJkYXRhIjp7Imhhc2giOnsiYWxnb3JpdGhtIjoic2hhMjU2IiwidmFsdWUiOiJlY2RjNTUzNmY3M2JkYWU4ODE2ZjBlYTQwNzI2ZWY1ZTliODEwZDkxNDQ5MzA3NTkwM2JiOTA2MjNkOTdiMWQ4In19LCJzaWduYXR1cmUiOnsiY29udGVudCI6Ik1FWUNJUUQvUGRQUW1LV0MxKzBCTkVkNWdLdlFHcjF4eGwzaWVVZmZ2M2prMXp6Skt3SWhBTEJqM3hmQXlXeGx6NGpwb0lFSVYxVWZLOXZua1VVT1NvZVp4QlpQSEtQQyIsImZvcm1hdCI6Ing1MDkiLCJwdWJsaWNLZXkiOnsiY29udGVudCI6IkxTMHRMUzFDUlVkSlRpQlFWVUpNU1VNZ1MwVlpMUzB0TFMwS1RVWnJkMFYzV1VoTGIxcEplbW93UTBGUldVbExiMXBKZW1vd1JFRlJZMFJSWjBGRlRVOWpWR1pTUWxNNWFtbFlUVGd4UmxvNFoyMHZNU3R2YldWTmR3cHRiaTh6TkRjdk5UVTJaeTlzY21sVE56SjFUV2haT1V4alZDczFWVW8yWmtkQ1oyeHlOVm80VERCS1RsTjFZWE41WldRNVQzUmhVblozUFQwS0xTMHRMUzFGVGtRZ1VGVkNURWxESUV0RldTMHRMUzB0Q2c9PSJ9fX19")
+	if err != nil {
+		t.Fatal(err)
+	}
+	entry := &pbs.TransparencyLogEntry{
+		LogIndex: 1,
+		InclusionProof: &pbs.InclusionProof{
+			LogIndex: 1,
+			TreeSize: 2,
+			Hashes: [][]byte{
+				[]byte(hash),
+			},
+		},
+		CanonicalizedBody: body,
+	}
+	checkpoint := &f_log.Checkpoint{
+		Size: 2,
+		Hash: rootHash,
+	}
+	gotErr := VerifyInclusionProof(entry, checkpoint)
+	assert.NoError(t, gotErr)
+}
+
+func TestVerifyCheckpoint(t *testing.T) {
+	hostname := "rekor.localhost"
+	treeSize := uint64(2)
+	sv, _, err := signature.NewDefaultECDSASignerVerifier()
+	if err != nil {
+		t.Fatal(err)
+	}
+	noteSigner, err := rekornote.NewNoteSigner(context.Background(), hostname, sv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	noteVerifier, err := rekornote.NewNoteVerifier(hostname, sv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootHash := sha256.Sum256([]byte{1, 2, 3})
+	cpRaw := f_log.Checkpoint{
+		Origin: hostname,
+		Size:   treeSize,
+		Hash:   rootHash[:],
+	}.Marshal()
+
+	n, err := note.Sign(&note.Note{Text: string(cpRaw)}, noteSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+	entry := &pbs.TransparencyLogEntry{
+		InclusionProof: &pbs.InclusionProof{
+			Checkpoint: &pbs.Checkpoint{
+				Envelope: string(n),
+			},
+		},
+	}
+	_, gotErr := VerifyCheckpoint(entry, noteVerifier)
+	assert.NoError(t, gotErr)
+}
