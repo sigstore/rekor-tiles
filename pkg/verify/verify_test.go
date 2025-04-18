01db9c2072fd0165420984b16f29d343aa98c5b3
Hayden B <8418760+haydentherapper@users.noreply.github.com>
2025-04-17 21:42:40 -0400

    Add consistency proof verification functions
    These will be used by log monitors and/or witnesses to verify
    consistency proofs, which the client is required to build.
    
    Fixes #197
    
    Signed-off-by: Hayden B <8418760+haydentherapper@users.noreply.github.com>

--- pkg/verify/verify_test.go
+++ pkg/verify/verify_test.go
@@ -18,6 +18,7 @@ package verify
 import (
 	"context"
 	"crypto/sha256"
+	"encoding/hex"
 	"testing"
 
 	pbs "github.com/sigstore/protobuf-specs/gen/pb-go/rekor/v1"
@@ -151,28 +152,28 @@ func TestVerifyCheckpoint(t *testing.T) {
 	}
 
 	for _, test := range []struct {
-		name    string
-		entry   *pbs.TransparencyLogEntry
-		wantErr bool
+		name       string
+		checkpoint string
+		wantErr    bool
 	}{
 		{
-			name:    "valid checkpoint",
-			entry:   getTestEntry(t, sv, hostname),
-			wantErr: false,
+			name:       "valid checkpoint",
+			checkpoint: getTestEntry(t, sv, hostname).GetInclusionProof().GetCheckpoint().GetEnvelope(),
+			wantErr:    false,
 		},
 		{
-			name:    "hostname mismatch",
-			entry:   getTestEntry(t, sv, "other.host"),
-			wantErr: true,
+			name:       "hostname mismatch",
+			checkpoint: getTestEntry(t, sv, "other.host").GetInclusionProof().GetCheckpoint().GetEnvelope(),
+			wantErr:    true,
 		},
 		{
-			name:    "signature mismatch",
-			entry:   getTestEntry(t, otherSigner, hostname),
-			wantErr: true,
+			name:       "signature mismatch",
+			checkpoint: getTestEntry(t, otherSigner, hostname).GetInclusionProof().GetCheckpoint().GetEnvelope(),
+			wantErr:    true,
 		},
 	} {
 		t.Run(string(test.name), func(t *testing.T) {
-			_, gotErr := VerifyCheckpoint(test.entry, noteVerifier)
+			_, gotErr := VerifyCheckpoint(test.checkpoint, noteVerifier)
 			if (gotErr != nil) != test.wantErr {
 				t.Fatalf("VerifyCheckpoint = %t, wantErr %t", gotErr, test.wantErr)
 			}
@@ -229,5 +230,61 @@ func TestVerifyLogEntry(t *testing.T) {
 	}
 
 	gotErr := VerifyLogEntry(entry, noteVerifier)
+	assert.NoError(t, gotErr)
+}
+
+func TestVerifyConsistencyProof(t *testing.T) {
+	hostname := "rekor.localhost"
+	root1, err := hex.DecodeString("59a575f157274702c38de3ab1e1784226f391fb79500ebf9f02b4439fb77574c")
+	if err != nil {
+		t.Fatal(err)
+	}
+	root2, err := hex.DecodeString("5be1758dd2228acfaf2546b4b6ce8aa40c82a3748f3dcb550e0d67ba34f02a45")
+	if err != nil {
+		t.Fatal(err)
+	}
+	consistencyHashes, err := hex.DecodeString("d3be742c8d73e2dd3c5635843e987ad3dfb3837616f412a07bf730c3ad73f5cb")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sv, _, err := signature.NewDefaultECDSASignerVerifier()
+	if err != nil {
+		t.Fatal(err)
+	}
+	noteVerifier, err := rekornote.NewNoteVerifier(hostname, sv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	noteSigner, err := rekornote.NewNoteSigner(context.Background(), hostname, sv)
+	if err != nil {
+		t.Fatal(err)
+
+	}
+
+	oldCpRaw := f_log.Checkpoint{
+		Origin: hostname,
+		Size:   1,
+		Hash:   root1,
+	}.Marshal()
+	oldCp, err := note.Sign(&note.Note{Text: string(oldCpRaw)}, noteSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newCpRaw := f_log.Checkpoint{
+		Origin: hostname,
+		Size:   2,
+		Hash:   root2,
+	}.Marshal()
+	newCp, err := note.Sign(&note.Note{Text: string(newCpRaw)}, noteSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gotErr := VerifyConsistencyProofWithCheckpoints([][]byte{consistencyHashes}, string(oldCp), string(newCp), noteVerifier)
+	assert.NoError(t, gotErr)
+
+	gotErr = VerifyConsistencyProof([][]byte{consistencyHashes}, 1, root1, string(newCp), noteVerifier)
 	assert.NoError(t, gotErr)
 }
