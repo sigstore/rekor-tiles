1c4bf8870730fe2223349ac0920b5dbbc8ce8777
Bob Callaway <bcallaway@google.com>
2025-05-14 14:27:44 -0400

    add log-level command line args, don't duplicate logs on http-grpc bridge
    Signed-off-by: Bob Callaway <bcallaway@google.com>

--- cmd/rekor-server/app/serve.go
+++ cmd/rekor-server/app/serve.go
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"time"
 
-	_ "github.com/chainguard-dev/clog/gcp/init" // initialize logging
+	clog "github.com/chainguard-dev/clog/gcp"
 	"k8s.io/klog/v2"
 
 	"github.com/spf13/cobra"
@@ -45,6 +45,13 @@ var serveCmd = &cobra.Command{
 	Long:  "start the Rekor server",
 	Run: func(cmd *cobra.Command, _ []string) {
 		ctx := cmd.Context()
+
+		logLevel := slog.LevelInfo
+		if err := logLevel.UnmarshalText([]byte(viper.GetString("log-level"))); err != nil {
+			slog.Error("invalid log-level specified; must be one of 'debug', 'info', 'error', or 'warn'")
+			os.Exit(1)
+		}
+		slog.SetDefault(slog.New(clog.NewHandler(logLevel)))
 
 		// tessera uses klog so pipe all klog messages to be written through slog
 		klog.SetSlogLogger(slog.Default())
@@ -132,6 +139,7 @@ var serveCmd = &cobra.Command{
 				server.WithGRPCTimeout(viper.GetDuration("timeout")),
 				server.WithGRPCMaxMessageSize(viper.GetInt("max-request-body-size")),
 				server.WithTLSCredentials(viper.GetString("tls-cert-file"), viper.GetString("tls-key-file")),
+				server.WithGRPCLogLevel(logLevel, viper.GetBool("request-response-logging")),
 			),
 			rekorServer,
 			shutdownFn,
@@ -149,6 +157,8 @@ func init() {
 	serveCmd.Flags().String("grpc-address", "127.0.0.1", "GRPC address to bind to")
 	serveCmd.Flags().Duration("timeout", 60*time.Second, "timeout")
 	serveCmd.Flags().Int("max-request-body-size", 4*1024*1024, "maximum request body size in bytes")
+	serveCmd.Flags().String("log-level", "info", "log level for the process. options are [debug, info, warn, error]")
+	serveCmd.Flags().Bool("request-response-logging", false, "enables logging of request and response content; log-level must be 'debug' for this to take effect")
 
 	// hostname
 	hostname, err := os.Hostname()
